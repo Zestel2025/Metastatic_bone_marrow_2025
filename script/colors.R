e5e12a1770e3cb980699e9a491f7a5663d8e20df
MBM <changzheng_xjr@163.com>
2026-05-10 16:15:22 +0800

    Create colors.R

---
+++ script/colors.R
@@ -0,0 +1,28 @@
+col_coarse = c("#A5303C", # T cell
+               # "#ECEF8A", # CD8
+               "#F17B28", # NK
+               "#6BBBDC", # B cell
+               "#57B894", # HSC
+               "#2474A6", # Myeloid
+               "#704F9D", # Stromal
+               "#A485BC" #Epi 
+               )
+col_Stromal = c("#304789","#CA6C61","#4CB9E2","#3E8635","#D70C1D","#8F7A75",
+                "#C24C21","#E47C25","#A2AC53","#266AB5","#F9D19F")
+# col_Endo = c("#304789","#CA6C61","#426FB7","#3E8635","#D70C1D")
+# col_MSC = c("#8F7A75","#C24C21","#C4DA5E","#A2AC53","#266AB5","#4CB9E2","#F9D19F",
+#             "#A71371","#268789","#E47C25")
+col_Myeloid = c("#E7D4B7","#2899D9","#3594AD",
+                "#88C654","#56BAA1","#CB6CA9",
+                "#855696","#99766F","#7084B2","#E93F23","#FF7E1F","#21FEB7",
+                "#D78B5E","#F0BFDD","#D15471","#BE0016","#FBFF0A","#81712B","#F1E889","#3958A7")
+col_Myeloid_HSC = c("#FFBF00",col_Myeloid)
+col_Tc_CD8 = c("#96CA63","#A287BC","#25639B","#CA96C2","#9AB9C4","#954729","#B63A8E",
+               "#5D5B95","#C8DB9B","#AE103D","#4A8A3F","#56BAA1","#BA9FA4","#252756")
+col_Tc_CD4 = c("#E9E893","#EEC9BE","#E58EA4","#82698D","#4E77BC","#86B6CE",
+               "#912552","#C1BCDE","#CF3A33","#F1753E","#C4DA5E")
+# col_Tc_CD8 = c("#398F37","#E05A5D","#CA1722","#533186","#F0A596","#BD89B4","#DA3E64","#F2A460",
+#                 "#EF6625","#DC6B7D","#7D5394","#BEBDDE","#2F85CB","#9A377F","#AD94C0","#236299")
+col_Bc = c("#B28A6B","#D890B9","#D6B75D","#D68C41",
+           "#A84036","#DEC5D2","#A45488","#53A166")
+col_Epi = c("#C8A6CE")
